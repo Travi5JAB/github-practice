0920937f295ecfd60a519b8ade86a24ce32a1ca5
learn <learn@learn18.localdomain>
2019-04-16 16:04:23 -0700

    ADDED IFS

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,9 +1,15 @@
 // Update the following function to take an argument and output 'Even' if the argument is an even number or 'Odd' if the number is an odd number.
 
-function oddOrEven(){
+function oddOrEven(num){
     console.log('result')
-}
 
+if(num  === 0){
+    console.log("Number is 0");
+}else if(num % 2 === 1){
+    console.log("odd");
+}else if (num % 2=== 0){
+    console.log("Even");
+}
 oddOrEven(4)
 oddOrEven(11)
 oddOrEven(75)
